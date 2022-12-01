7bb50229b6ebff5c67dbf1a94432abb32e7c1011
sampr23 <sampr2323@gmail.com>
2022-12-01 15:34:54 -0500

    added the video in the background

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,17 @@
 function vote(answer)
 {
     surrenderBoxVote = document.getElementsByClassName("vote-unvoted")[0];
+    secondBox = document.getElementsByClassName("vote-unvoted")[1];
     if(answer == true)
+    {
         surrenderBoxVote.className = "vote-yes";
+        setTimeout(function(){secondBox.className = "vote-no";}, 1000);
+    }
     else
+    {
         surrenderBoxVote.className = "vote-no";
+        setTimeout(function(){secondBox.className = "vote-yes";}, 1000);
+    }
 
     hideSurrenderButton(answer);
 }
@@ -17,6 +24,13 @@ function hideSurrenderButton(answer)
     surrenderText.style.display = "block";
 
     voteResult = document.getElementById("vote-result");
-
     voteResult.innerHTML = answer ? "You voted yes." : "You voted no.";
+
+    playVideo();
+}
+
+function playVideo()
+{
+    var video = document.getElementById("myVideo");
+    video.play();
 }
